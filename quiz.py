d03c14ecac0805f2fb8e49dacffcd36fcb618d54
Michaelexe <michaelispahani@gmail.com>
2020-07-18 17:54:22 +0430

    pretty much the final product

--- quiz.py
+++ quiz.py
@@ -3,18 +3,59 @@ import tkinter.font as tkfont
 from time import sleep
 
 
+
+result = 0
+nextclick = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#necessary functions
+
 def delete():
+	global nextclick
 	for widget in overlaybg.winfo_children():
 		widget.destroy()
 
 	for widget in mainbg.winfo_children():
 		widget.destroy()
 
-	question2()
+	nextclick += 1
 
+	if nextclick == 0:
+		question1()
 
+	if nextclick == 1:
+		question2()
+
+	if nextclick == 2:
+		question3()
+
+	if nextclick == 3:
+		question4()
+
+	if nextclick == 4:
+		question5()
 
 def right_answer():
+	global result
+
+	result += 1
+
 	fontsize_right = tkfont.Font(size = 20)
 	rightlabel = tk.Label(overlaybg, text = "YOU ARE CORRECT!!", fg = "Green", bg = "White",font = fontsize_right)
 	rightlabel.place(relx = 0.3, rely = 0.8)
@@ -31,6 +72,24 @@ def wrong_answer():
 	nextbutton.place(relwidth = 0.1, relx = 0.45, rely = 0.9)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Question1
 def question1():
 
 	fontsize = tkfont.Font(size = 20)
@@ -48,6 +107,9 @@ def question1():
 	option3.place(relwidth = 0.2, relx = 0.4, rely = 0.5)
 	option4.place(relwidth = 0.2, relx = 0.4, rely = 0.6)
 
+
+#Question2
+
 def question2():
 	fontsize = tkfont.Font(size = 20)
 	q1 = tk.Label(overlaybg, text = "What is the hottest substance in the universe?", fg = "Black", bg = "White", font = fontsize)
@@ -64,14 +126,92 @@ def question2():
 	option3.place(relwidth = 0.2, relx = 0.4, rely = 0.5)
 	option4.place(relwidth = 0.2, relx = 0.4, rely = 0.6)
 
+#Question3
+
+def question3():
+	fontsize = tkfont.Font(size = 20)
+	q1 = tk.Label(overlaybg, text = "What comes between Mars and Jupiter?", fg = "Black", bg = "White", font = fontsize)
+	q1.place(relx = 0.1, rely = 0.1)
+
+
+	option1 = tk.Button(overlaybg, text = "a) The Astroid Belt", fg = "Black", bg = "Grey", command = right_answer)
+	option2 = tk.Button(overlaybg, text = "b) Saturn", fg = "Black", bg = "Grey", command = wrong_answer)
+	option3 = tk.Button(overlaybg, text = "c) Nothing", fg = "Black", bg = "Grey", command = wrong_answer)
+	option4 = tk.Button(overlaybg, text = "d) Earth", fg = "Black", bg = "Grey", command = wrong_answer)
+
+	option1.place(relwidth = 0.2, relx = 0.4, rely = 0.3)
+	option2.place(relwidth = 0.2, relx = 0.4, rely = 0.4)
+	option3.place(relwidth = 0.2, relx = 0.4, rely = 0.5)
+	option4.place(relwidth = 0.2, relx = 0.4, rely = 0.6)
+
+#Question4
+
+def question4():
+	fontsize = tkfont.Font(size = 20)
+	q1 = tk.Label(overlaybg, text = "which blackhole is at the center of our galaxy?", fg = "Black", bg = "White", font = fontsize)
+	q1.place(relx = 0.03, rely = 0.1)
+
+
+	option1 = tk.Button(overlaybg, text = "a) Sagitarius A-star", fg = "Black", bg = "Grey", command = right_answer)
+	option2 = tk.Button(overlaybg, text = "b) Alpha centauri", fg = "Black", bg = "Grey", command = wrong_answer)
+	option3 = tk.Button(overlaybg, text = "c) Milky Way", fg = "Black", bg = "Grey", command = wrong_answer)
+	option4 = tk.Button(overlaybg, text = "d) Adromeda", fg = "Black", bg = "Grey", command = wrong_answer)
+
+	option1.place(relwidth = 0.2, relx = 0.4, rely = 0.3)
+	option2.place(relwidth = 0.2, relx = 0.4, rely = 0.4)
+	option3.place(relwidth = 0.2, relx = 0.4, rely = 0.5)
+	option4.place(relwidth = 0.2, relx = 0.4, rely = 0.6)
+
+#Question5
+
+def question5():
+	fontsize = tkfont.Font(size = 20)
+	q1 = tk.Label(overlaybg, text = "Which galaxy is likely to collide with our galaxy in the future?", fg = "Black", bg = "White", font = fontsize)
+	q1.place(relx = 0.03, rely = 0.1)
+
+
+	option1 = tk.Button(overlaybg, text = "a) Milky Way", fg = "Black", bg = "Grey", command = wrong_answer)
+	option2 = tk.Button(overlaybg, text = "b) Kepler 250", fg = "Black", bg = "Grey", command = wrong_answer)
+	option3 = tk.Button(overlaybg, text = "c) Sagitarius", fg = "Black", bg = "Grey", command = wrong_answer)
+	option4 = tk.Button(overlaybg, text = "d) Adromeda", fg = "Black", bg = "Grey", command = right_answer)
+
+	option1.place(relwidth = 0.2, relx = 0.4, rely = 0.3)
+	option2.place(relwidth = 0.2, relx = 0.4, rely = 0.4)
+	option3.place(relwidth = 0.2, relx = 0.4, rely = 0.5)
+	option4.place(relwidth = 0.2, relx = 0.4, rely = 0.6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 root = tk.Tk()
 
-mainbg = tk.Canvas(root, height = 450, width = 800, bg = "Red")
+mainbg = tk.Canvas(root, height = 450, width = 800, bg = "Blue")
 mainbg.pack()
 
 overlaybg = tk.Frame(root, bg = "White")
 overlaybg.place(relheight = 0.8, relwidth = 0.8, relx = 0.1, rely = 0.1)
+
 
 question1()
 
